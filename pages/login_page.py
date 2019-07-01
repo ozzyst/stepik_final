fac5819f134068ee3b4795422427fd8a02f8c9b8
s.losev <s.losev@yadro.com>
2019-07-01 10:31:06 +0300

    Fixed login

--- pages/login_page.py
+++ pages/login_page.py
@@ -20,15 +20,12 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert 'login' in self.browser.current_url, "It's not login url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM),\
                'There is no login form on this page'
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM),\
                'There is no register form on this page'
